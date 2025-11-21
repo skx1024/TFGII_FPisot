a83850e650c653ccc49ecdb8b09abb96e8c8f0c6
Catalina Vicol <cvx1007@alu.ubu.es>
2025-11-21 20:28:23 +0100

    Refactorización summary_screen para eliminar issue SQ

--- project-app/project_app/lib/screens/tour_summary_screen.dart
+++ project-app/project_app/lib/screens/tour_summary_screen.dart
@@ -17,177 +17,205 @@ class TourSummaryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final cardWidth =
-        screenWidth * 0.9; // Define el ancho de la tarjeta principal.
+    final cardWidth = screenWidth * 0.9;
 
     return BlocBuilder<TourBloc, TourState>(
       builder: (context, state) {
-        // Verifica si el Eco City Tour no existe en el estado actual.
         if (state.ecoCityTour == null) {
-          // Muestra un Snackbar y navega atrás cuando el tour es nulo.
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            CustomSnackbar.show(context, 'empty_tour_message'.tr());
-            Navigator.pop(context);
-          });
+          _handleEmptyTour(context);
           return const SizedBox.shrink();
         }
 
-        // Construcción de la pantalla principal
         return Scaffold(
-          appBar: AppBar(
-            iconTheme:
-                const IconThemeData(color: Colors.white), // Iconos en blanco
-            centerTitle: true,
-            title: Text(
-              'tour_summary_title'.tr(),
-              style: const TextStyle(color: Colors.white),
-            ),
-            backgroundColor: Theme.of(context).primaryColor, // Color del AppBar
-            actions: [
-              // Botón para guardar el tour
-              IconButton(
-                icon: const Icon(Icons.save_as_rounded),
-                tooltip: 'save_tour_tooltip'.tr(),
-                onPressed: () async {
-                  // Abre un diálogo para solicitar el nombre del tour
-                  final tourName = await showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      String inputText = '';
-                      return AlertDialog(
-                        title: Text('save_tour_name'.tr()),
-                        content: TextField(
-                          onChanged: (value) => inputText = value,
-                          decoration: InputDecoration(
-                              hintText: "save_tour_placeholder".tr()),
-                        ),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, inputText),
-                            child: Text('save_button'.tr()),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-
-                  // Guarda el tour si el nombre es válido
-                  if (tourName != null &&
-                      tourName.isNotEmpty &&
-                      context.mounted) {
-                    await BlocProvider.of<TourBloc>(context)
-                        .saveCurrentTour(tourName);
-                    if (context.mounted) {
-                      CustomSnackbar.show(context, 'tour_saved_success'.tr());
-                    }
-                  }
-                },
-              ),
-            ],
-          ),
+          appBar: _buildAppBar(context),
           body: Column(
             children: [
-              // Tarjeta que muestra los detalles principales del tour
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: Center(
-                  child: SizedBox(
-                    width:
-                        cardWidth, // Ajusta el tamaño al 90% del ancho de la pantalla
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Información de la ciudad
-                            Text(
-                              '${'city'.tr()}: ${state.ecoCityTour!.city}',
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-
-                            // Información de la distancia total
-                            Text(
-                              '${'distance'.tr()}: ${formatDistance(state.ecoCityTour!.distance ?? 0)}',
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(height: 4),
-
-                            // Información de la duración total
-                            Text(
-                              '${'duration'.tr()}: ${formatDuration((state.ecoCityTour!.duration ?? 0).toInt())}',
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(height: 4),
-
-                            // Información del medio de transporte
-                            Row(
-                              children: [
-                                Text('${'transport_mode'.tr()}:',
-                                    style: const TextStyle(fontSize: 16)),
-                                const SizedBox(width: 8),
-                                Icon(
-                                  transportIcons[state.ecoCityTour!.mode],
-                                  size: 24,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 8),
-                                                        // Gustos del usuario (íconos pequeños)
-                            if (state.ecoCityTour!.userPreferences.isNotEmpty)
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: state.ecoCityTour!.userPreferences
-                                    .map((preference) {
-                                  final prefIconData =
-                                      userPreferences[preference];
-                                  if (prefIconData != null) {
-                                    return Padding(
-                                      padding:
-                                          const EdgeInsets.only(right: 8.0),
-                                      child: Icon(
-                                        prefIconData['icon'],
-                                        color: prefIconData['color'],
-                                        size: 24, // Tamaño pequeño
-                                      ),
-                                    );
-                                  } else {
-                                    return const SizedBox.shrink();
-                                  }
-                                }).toList(),
-                              ),
-                            const SizedBox(height: 8),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              // Lista de puntos de interés (POIs)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.ecoCityTour!.pois.length,
-                  itemBuilder: (context, index) {
-                    final poi = state.ecoCityTour!.pois[index];
-                    return ExpandablePoiItem(
-                        poi: poi, tourBloc: BlocProvider.of<TourBloc>(context));
-                  },
-                ),
-              ),
+              _buildSummaryCard(context, state, cardWidth),
+              _buildPoiList(context, state),
             ],
           ),
         );
       },
+    );
+  }
+
+  /// Maneja el caso cuando el tour es nulo.
+  void _handleEmptyTour(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      CustomSnackbar.show(context, 'empty_tour_message'.tr());
+      Navigator.pop(context);
+    });
+  }
+
+  /// Construye el AppBar.
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      iconTheme: const IconThemeData(color: Colors.white),
+      centerTitle: true,
+      title: Text(
+        'tour_summary_title'.tr(),
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Theme.of(context).primaryColor,
+      actions: [_buildSaveTourButton(context)],
+    );
+  }
+
+  /// Construye el botón para guardar el tour.
+  Widget _buildSaveTourButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.save_as_rounded),
+      tooltip: 'save_tour_tooltip'.tr(),
+      onPressed: () => _showSaveTourDialog(context),
+    );
+  }
+
+  /// Muestra el diálogo para guardar el tour.
+  Future<void> _showSaveTourDialog(BuildContext context) async {
+    final tourName = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        String inputText = '';
+        return AlertDialog(
+          title: Text('save_tour_name'.tr()),
+          content: TextField(
+            onChanged: (value) => inputText = value,
+            decoration: InputDecoration(hintText: "save_tour_placeholder".tr()),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, inputText),
+              child: Text('save_button'.tr()),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (tourName != null && tourName.isNotEmpty && context.mounted) {
+      await BlocProvider.of<TourBloc>(context).saveCurrentTour(tourName);
+      if (context.mounted) {
+        CustomSnackbar.show(context, 'tour_saved_success'.tr());
+      }
+    }
+  }
+
+  /// Construye la tarjeta de resumen del tour.
+  Widget _buildSummaryCard(
+      BuildContext context, TourState state, double cardWidth) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: Center(
+        child: SizedBox(
+          width: cardWidth,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildCityInfo(state),
+                  const SizedBox(height: 8),
+                  _buildDistanceInfo(state),
+                  const SizedBox(height: 4),
+                  _buildDurationInfo(state),
+                  const SizedBox(height: 4),
+                  _buildTransportModeInfo(context, state),
+                  const SizedBox(height: 8),
+                  _buildUserPreferencesIcons(state),
+                  const SizedBox(height: 8),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Construye la información de la ciudad.
+  Widget _buildCityInfo(TourState state) {
+    return Text(
+      '${'city'.tr()}: ${state.ecoCityTour!.city}',
+      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    );
+  }
+
+  /// Construye la información de la distancia.
+  Widget _buildDistanceInfo(TourState state) {
+    return Text(
+      '${'distance'.tr()}: ${formatDistance(state.ecoCityTour!.distance ?? 0)}',
+      style: const TextStyle(fontSize: 16),
+    );
+  }
+
+  /// Construye la información de la duración.
+  Widget _buildDurationInfo(TourState state) {
+    return Text(
+      '${'duration'.tr()}: ${formatDuration((state.ecoCityTour!.duration ?? 0).toInt())}',
+      style: const TextStyle(fontSize: 16),
+    );
+  }
+
+  /// Construye la información del modo de transporte.
+  Widget _buildTransportModeInfo(BuildContext context, TourState state) {
+    return Row(
+      children: [
+        Text('${'transport_mode'.tr()}:', style: const TextStyle(fontSize: 16)),
+        const SizedBox(width: 8),
+        Icon(
+          transportIcons[state.ecoCityTour!.mode],
+          size: 24,
+          color: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
+  }
+
+  /// Construye los íconos de preferencias de usuario.
+  Widget _buildUserPreferencesIcons(TourState state) {
+    if (state.ecoCityTour!.userPreferences.isEmpty) return const SizedBox.shrink();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: state.ecoCityTour!.userPreferences.map((preference) {
+        return _buildPreferenceIcon(preference);
+      }).toList(),
+    );
+  }
+
+  /// Construye un ícono de preferencia individual.
+  Widget _buildPreferenceIcon(String preference) {
+    final prefIconData = userPreferences[preference];
+    if (prefIconData != null) {
+      return Padding(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Icon(
+          prefIconData['icon'],
+          color: prefIconData['color'],
+          size: 24,
+        ),
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  /// Construye la lista de puntos de interés.
+  Widget _buildPoiList(BuildContext context, TourState state) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: state.ecoCityTour!.pois.length,
+        itemBuilder: (context, index) {
+          final poi = state.ecoCityTour!.pois[index];
+          return ExpandablePoiItem(
+            poi: poi,
+            tourBloc: BlocProvider.of<TourBloc>(context),
+          );
+        },
+      ),
     );
   }
 }
